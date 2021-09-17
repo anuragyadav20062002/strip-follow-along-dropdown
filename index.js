1653da964f11acc332bdb25a456e0ae1f6dc4b41
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-09-18 04:44:52 +0530

    added dropdown

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const nav = document.querySelector('.top')
 
 function handleEnter(){
 console.log("Enter");
+this.classList.add('trigger-enter');
+
+setTimeout(()=>{
+    this.classList.add('trigger-enter-active')
+}, 150)
 }
 
 function handleLeave(){
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
 console.log("leave")
 }
 
