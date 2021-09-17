2748a4605eb3c2ea969783a6b552cde58e1e2784
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-09-18 05:02:29 +0530

    added background div

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ const background = document.querySelector('.dropdownBackground')
 const nav = document.querySelector('.top')
 
 function handleEnter(){
-console.log("Enter");
+
 this.classList.add('trigger-enter');
 
 setTimeout(()=>{
     this.classList.add('trigger-enter-active')
 }, 150)
+
+background.classList.add('open');
+
+const dropdown = this.querySelector('.dropdown');
+const dropdownCoords = dropdown.getBoundingClientRect();
+const navcoords = nav.getBoundingClientRect();
+
+const cords={
+    width: dropdownCoords.width,
+    height: dropdownCoords.height,
+    top: dropdownCoords.top-navcoords.top,
+    left: dropdownCoords.left-navcoords.left
+}
+
+background.style.setProperty('width', `${cords.width}px`);
+background.style.setProperty('height', `${cords.height}px`);
+background.style.setProperty('transform', `translate(${cords.left}px, ${cords.top}px)`)
 }
 
 function handleLeave(){
     this.classList.remove('trigger-enter', 'trigger-enter-active');
-console.log("leave")
+    background.classList.remove('open');
+
 }
 
 triggers.forEach(trigger=> trigger.addEventListener('mouseenter', handleEnter));
